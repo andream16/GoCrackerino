5e1ff51cb80f51760b9481cd5b43efd4140d358c
andream16 <andrea.medda.uni@gmail.com>
2016-10-25 22:20:16 +0200

    Working goroutines

--- cracker-core/hello.go
+++ cracker-core/hello.go
@@ -2,26 +2,48 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-	"strconv"
+	"sync"
 )
 
 
 func main() {
-	var (
-		cmdOut []byte
-		err    error
-	)
 
-	fmt.Println(strconv.FormatInt(10, 16))
-
+	//Bash Command
 	cmd  := "./encrypt"
+	//Arguments to get passed to the command
 	args := []string{"-s", "000000", "FFC7C9E5694ABFF7"}
-	if cmdOut, err = exec.Command(cmd, args...).Output(); err != nil {
-		fmt.Fprintln(os.Stderr, "There was an error running "+cmd+" "+args[0]+args[1]+args[2], err)
-		os.Exit(1)
+
+	//Common Channel for the goroutines
+	tasks := make(chan *exec.Cmd, 64)
+
+	//Spawning 4 goroutines
+	var wg sync.WaitGroup
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func(num int, w *sync.WaitGroup) {
+			defer w.Done()
+			var (
+				out []byte
+				err error
+			)
+			for cmd := range tasks { // this will exit the loop when the channel closes
+				out, err = cmd.Output()
+				if err != nil {
+					fmt.Printf("can't get stdout:", err)
+				}
+				fmt.Printf("%s", string(out))
+			}
+		}(i, &wg)
 	}
-	sha := string(cmdOut)
-	fmt.Println(sha)
+	//Generate Tasks
+	for i := 0; i < 100000; i++ {
+		tasks <- exec.Command(cmd, args...)
+	}
+	close(tasks)
+
+	// wait for the workers to finish
+	wg.Wait()
+
+	fmt.Println("Done")
 }
