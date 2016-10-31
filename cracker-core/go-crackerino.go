ee99f095e25f59c1550fc5ffafc229e3a2d2d291
andream16 <andrea.medda.uni@gmail.com>
2016-10-31 23:08:04 +0100

    Done 28-bit

--- cracker-core/go-crackerino.go
+++ cracker-core/go-crackerino.go
@@ -8,12 +8,14 @@ import (
 	"sync"
 )
 
+//Error handling
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+//Gets Max number of goroutines to get Called
 func MaxParallelism() int {
 	maxProcs := runtime.GOMAXPROCS(0)
 	numCPU := runtime.NumCPU()
@@ -32,47 +34,70 @@ func main() {
 	akwArgs := []string{"x[$2]++ == 1 { print $2 }", "store.txt"}
 	grepCmd := "grep"
 	grepArg := "store.txt"
-	//Arguments to get passed to the encryption/decryption
-	args := []string{"-s", "C330C9CBD01DFBA0", "E10C65124518DB05"}
+	//Arguments to get passed to the encryption/decryption, change plainText and cipherText with yours
+	args := []string{"-s", "yourPlainTex", "yourCipherText"}
 
+	//Create a new file f
 	f, err := os.Create("store.txt")
 	check(err)
+	//Defer Close in make sure we have done writing
 	defer f.Close()
 
 	//Common Channel for the goroutines
 	tasks := make(chan *exec.Cmd, 64)
 
-	//Spawning 8 goroutines
+	//Create a WaitGroup for the goroutines
 	var wg sync.WaitGroup
+	//Getting Max number of goroutines
 	cores := MaxParallelism()
+	//Spawning MAX_Number goroutines
 	for i := 0; i < cores; i++ {
+		//Add a new gorotuine to the Waitgroup
 		wg.Add(1)
+		//Call a new Goroutine
 		go func(num int, w *sync.WaitGroup) {
+			//Until done
 			defer w.Done()
+			//Initialize a byte container
 			var (
 				out []byte
 				err error
 			)
+			//Execute a passed Command
 			for cmd := range tasks {
+				//Get its output
 				out, err = cmd.Output()
+				//If error
 				if err != nil {
 					fmt.Printf("Can't get stdout:", err)
 				}
+				//Cast output to string
 				s:= string(out)
+				//Write the result on the file: key cipherText
 				f.WriteString(cmd.Args[2]+" "+s)
+				//Sync R/W on f
 				f.Sync()
 			}
 		}(i, &wg)
 	}
-	//Generate Tasks
-	for i := 0; i < 16777215; i++ {
-		key := string(fmt.Sprintf("%06x", i))
+	/** Generate as many tasks as we need, 24-bit: 16777216, 28-bit: 268435456, 32-bit: 4294967296
+	  *  goroutines will communicate between each other and pick one different task per time
+	  *  CPU and Core utilization is managed by Go. The more tasks we have the more goroutines will be used.
+	  *  Go scales based on the amount of tasks to do.
+	**/
+	for i := 0; i < 268435456; i++ {
+		//Replace ?? with your preferred padding. 24-bit: 06x, 28-bit: 07x, 32-bit: 08x
+		key := string(fmt.Sprintf("%??x", i))
+		//Execute Encrypt
 		tasks <- exec.Command(cmdEnc, args[0], key, args[1])
+		//Execute Decrypt
 		tasks <- exec.Command(cmdDec, args[0], key, args[2])
 	}
+	//When Done, close tasks
 	close(tasks)
 	// Wait for the workers to finish
 	wg.Wait()
+	//When all the workers are done, close the file
 	f.Close()
 	fmt.Println("Done Writing Keys")
 
